be44ee66bff50075cf0366016016e44c824cf69f
soloman-err <soloman.err@gmail.com>
2024-04-04 03:52:22 +0600

    style(layout): header layout integrate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Header } from "@/components/shared/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import { Header } from "@/components/shared/header/Header";
+
 export default function Home() {
   return (
     <main className="container text-center mx-auto">
-      <h1 className="text-3xl font-semibold mb-4">
-        School Management Software
-      </h1>
+      <h1 className="text-3xl font-semibold mb-4">...</h1>
     </main>
   );
 }
